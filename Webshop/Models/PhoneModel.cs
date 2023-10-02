9d4058176a11a0407957d04281200467063ddf4d
515670 <515670@student.fontys.nl>
2023-10-02 19:54:50 +0200

    Add project files.

---
+++ Webshop/Models/PhoneModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Webshop.Models
+{
+    public class PhoneModel
+    {
+        public string ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+
+
+    }
+}

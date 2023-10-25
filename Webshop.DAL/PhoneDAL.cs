2b57b0b0acfb8364beb973d85c79ee9cc4f97c28
Osck53 <oscarkaluza1@gmail.com>
2023-10-25 18:39:18 +0200

    added addphone query and connection

--- Webshop.DAL/PhoneDAL.cs
+++ Webshop.DAL/PhoneDAL.cs
@@ -1,22 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Numerics;
+using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Webshop.DAL
 {
     public class PhoneDAL
     {
-        public string connectionString = "";
+        public string connectionString = "Server=studmysql01.fhict.local; Port=3306;Database=dbi515670;User=dbi515670;Password=Tua1X#TbOS;";
+
 
         public void AddPhone(int id, string brand, string model, string description, int price)
         {
-            //Phone phone = new Phone(1, "Samsung", "fjdsg", "ITUH", 21);
-            string sqlQuery = "";
+            string sqlQuery = "INSERT INTO `phone`(`ID`, `Brand`, `Model`, `Description`, `Price`) VALUES(id, brand, model, description, price)";
 
-            int test = id;
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(sqlQuery, connection))
+                {
+                    command.Parameters.AddWithValue("id", id);
+                    command.Parameters.AddWithValue("brand", brand);
+                }
+            }
+
         }
 
 

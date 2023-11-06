d71820ffc83f65d6c95f9f952178088e062b2df7
osck53 <osck53@OSCARKALUZA6258>
2023-11-06 15:38:12 +0100

    Made start to ordeView and ordercontroller

---
+++ Webshop/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Webshop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+
+namespace Webshop.Controllers
+{
+    public class OrderController : Controller
+    {
+        public OrderModel ordermodel { get; set; }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Order()
+        {
+            // Not working
+            return new string($"Phone with ID {ordermodel.ID} has been ordered");
+        }
+
+        [HttpPost]
+        public IActionResult Order(int ID)
+        {
+            ordermodel = new OrderModel();
+            ordermodel.ID = ID;
+
+            return View("Index");
+        }
+    }
+}
---
+++ Webshop/Models/OrderModel.cs
@@ -0,0 +1,9 @@
+﻿using Webshop.BLL.Phone;
+
+namespace Webshop.Models
+{
+    public class OrderModel
+    {
+        public int ID { get; set; }
+    }
+}
